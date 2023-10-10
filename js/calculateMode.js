32b702b06dbb34df4e7f11a8e243079e3a767c54
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-09 20:45:15 -0500

    JS and python solved

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,20 @@
-const calculateMode = () => {
+exports.calculateMode = function(arr) {
+    const answer = []
+    let freq = 0
+    let obj = {}
+    
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        obj[item] = (obj[item] || 0) + 1;
+        if (obj[item] > freq) {
+            freq = obj[item];
+        }
+    }
 
+    for (const key in obj) {
+        if (obj[key] === freq) {
+            answer.push(key);
+        }
+    }
+    return answer
 }
--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,14 @@
-def calculate_mode():
-    pass
+def calculate_mode(lst):
+    answer =[]
+    freq = 0
+    my_dict = {}
+    for i in lst:
+        times = lst.count(i)
+        my_dict[i] = times
+        if times > freq:
+            freq = times
+
+    for i in my_dict:
+        if my_dict[i] == freq:
+            answer.append(i)
+    return answer 
